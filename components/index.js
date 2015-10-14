99a9404e3749c25acca702acd3997d507f72fec2
Tyler Peterson <petersontb@familysearch.org>
2015-10-14 10:48:06 -0600

    Complete handed-off requests
    While harvesting hand-off data initiate ajax requests to complete work for handed-off data.
    Make render of the below-the-fold component a function and call it every time new results come back.

--- components/index.js
+++ components/index.js
@@ -7,7 +7,7 @@ var beaconId = 0;
 var handedOffData = [];
 var attachPoint = document.getElementById('below-the-fold');
 
-function retrieveBeaconId() { // Retrieve the beaconId
+(function () { // Retrieve the beaconId
   var metas = document.getElementsByTagName('meta');
   var i, l = metas.length;
   for (i=0; i < l; ++i) {
@@ -16,25 +16,48 @@ function retrieveBeaconId() { // Retrieve the beaconId
       break;
     }
   }
-}
-
-retrieveBeaconId();
+})()
 
 function retrieveHandoffData() {
   var statusElements = attachPoint.getElementsByClassName('status');
-  var i, d, l = statusElements.length;
+  var i, d, proj, l = statusElements.length;
+
+  function completeHandoff(proj) {
+    var req;
+
+    console.debug('completing handoff for ' + proj.promiseId);
+    req = new XMLHttpRequest();
+    req.addEventListener('load', function () {
+      console.debug('Got results', proj.promiseId, req.responseText);
+      proj.count = req.responseText;
+      proj.status = 'done';
+      renderBelow();
+    });
+    req.open('GET', '/project?id=' + proj.promiseId);
+    req.send();
+  }
+
   for (i=0; i < l; ++i) {
     d = statusElements[i].dataset;
-    handedOffData.push({
+    proj = {
       name: d.name + '-',
       count: d.count,
       status: d.status,
       promiseId: d.promiseId
-    });
+    };
+
+    handedOffData.push(proj);
+
+    if (proj.status !== 'done') {
+      completeHandoff(proj);
+    }
   }
 }
 
 retrieveHandoffData();
 
-var belowComponent = ReactDom.render(React.createElement(Below, {data: handedOffData}), attachPoint);
+function renderBelow() {
+  ReactDom.render(React.createElement(Below, {data: handedOffData}), attachPoint);
+}
 
+renderBelow();
