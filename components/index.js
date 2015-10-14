14b2c5ad4a27b58596b5763afb83e1990ac0e1a0
Tyler Peterson <petersontb@familysearch.org>
2015-10-14 10:54:02 -0600

    Remove unused code in index

--- components/index.js
+++ components/index.js
@@ -2,40 +2,13 @@
 
 var ReactDom = require('react-dom');
 var Below = require('./below');
-var data = require('../app/project-data');
-var beaconId = 0;
+
 var handedOffData = [];
 var attachPoint = document.getElementById('below-the-fold');
-
-(function () { // Retrieve the beaconId
-  var metas = document.getElementsByTagName('meta');
-  var i, l = metas.length;
-  for (i=0; i < l; ++i) {
-    if (metas[i].getAttribute('name') == 'beaconId') {
-      beaconId = metas[i].getAttribute('content');
-      break;
-    }
-  }
-})()
 
 function retrieveHandoffData() {
   var statusElements = attachPoint.getElementsByClassName('status');
   var i, d, proj, l = statusElements.length;
-
-  function completeHandoff(proj) {
-    var req;
-
-    console.debug('completing handoff for ' + proj.promiseId);
-    req = new XMLHttpRequest();
-    req.addEventListener('load', function () {
-      console.debug('Got results', proj.promiseId, req.responseText);
-      proj.count = req.responseText;
-      proj.status = 'done';
-      renderBelow();
-    });
-    req.open('GET', '/project?id=' + proj.promiseId);
-    req.send();
-  }
 
   for (i=0; i < l; ++i) {
     d = statusElements[i].dataset;
@@ -54,10 +27,24 @@ function retrieveHandoffData() {
   }
 }
 
-retrieveHandoffData();
+function completeHandoff(proj) {
+  var req;
+
+  console.debug('completing handoff for ' + proj.promiseId);
+  req = new XMLHttpRequest();
+  req.addEventListener('load', function () {
+    console.debug('Got results', proj.promiseId, req.responseText);
+    proj.count = req.responseText;
+    proj.status = 'done';
+    renderBelow();
+  });
+  req.open('GET', '/project?id=' + proj.promiseId);
+  req.send();
+}
 
 function renderBelow() {
   ReactDom.render(React.createElement(Below, {data: handedOffData}), attachPoint);
 }
 
+retrieveHandoffData();
 renderBelow();
