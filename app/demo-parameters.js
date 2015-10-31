390f13f4b9e678ee1c078d29d8d567955912bf4d
Tyler Peterson <petersontb@familysearch.org>
2015-10-31 16:13:45 -0600

    Check in code walkthrough

--- app/demo-parameters.js
+++ app/demo-parameters.js
@@ -4,8 +4,8 @@ module.exports = {
     return 1500 + Math.round(Math.random() * 1500); // slower connection
   },
   projectDataLatency: function () {
-    // return 50 + Math.round(Math.random() * 10000); // fast to very slow responses
-    return 500 + Math.round(Math.random() * 100); // mostly medium responses
+    return 50 + Math.round(Math.random() * 10000); // fast to very slow responses
+    // return 500 + Math.round(Math.random() * 100); // mostly medium responses
   },
   clientSideJS: true // Set to false to prevent downloading the bundled JS
 };
